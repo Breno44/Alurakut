9daf8448aea73dc3d2557b9eda7dc1abc75c05cf
Breno Henrique <bhfs.2908@gmail.com>
2021-08-30 18:12:00 -0300

    feat:ending class 01

---
+++ src/components/MainGrid/index.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+export const MainGrid = styled.main`
+  display: grid;
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 500px;
+  grid-gap: 10px;
+  padding: 16px;
+
+  .profileArea {
+    display: none;
+    @media (min-width: 860px) {
+      display: block;
+    }
+  }
+
+  @media (min-width: 860px) {
+    max-width: 1110px;
+    display: grid;
+    grid-template-areas: "profileArea welcomeArea profileRelationsArea";
+    grid-template-columns: 160px 1fr 312px;
+  }
+`;
