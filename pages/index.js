83202d5f949779c52aa9678d388e356dcc224f0b
Breno Henrique <bhfs.2908@gmail.com>
2021-08-30 15:50:06 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `;
+const Box = styled.h1`
+  background: #fff;
+  border-radius: 8px;
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <main>
+      <Box>imagens</Box>
+      <Box>Bem vindo</Box>
+      <Box>comunidades</Box>
+    </main>
+  );
 }
